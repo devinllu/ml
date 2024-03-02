4145dc91f69fa7bc095db7a66a41a47c268fc183
Devin <devinllu@hotmail.com>
2024-03-01 17:35:25 -0800

    feat: trained RFC on wine data, ~69% accuracy

--- hw1.py
+++ hw1.py
@@ -1,17 +1,33 @@
-from sklearn.datasets import load_wine
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score
 
 import pandas as pd
 import numpy as np
 
 def sklearn_to_df(sklearn_dataset):
     df = pd.DataFrame(sklearn_dataset.data, columns=sklearn_dataset.feature_names)
-    df['target'] = pd.Series(sklearn_dataset.target)
     return df
 
+def prepocess_data(df):
+    df.fillna(df.mean(), inplace=True)
 
-data = load_wine()
+    scaler = StandardScaler()
+    return scaler.fit_transform(df.drop('quality', axis=1))
 
-wine_data = sklearn_to_df(data)
+data = pd.read_csv('data/wine_quality.csv')
+wine_data_scaled = prepocess_data(data)
 
-print(wine_data.head())
+x, y = wine_data_scaled, data['quality']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(random_state=42)
+
+model.fit(x_train, y_train)
+
+predictions = model.predict(x_test)
+print(f"Accuracy: {accuracy_score(y_test, predictions)}")
+
